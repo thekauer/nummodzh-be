9e4bb2830932099b2f22ea006a84082c57436d45
thekauer <thekauer@gmail.com>
2022-06-25 18:37:22 +0200

    feat: :lock: enable cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
-
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('Nummod ZH API')
     .setDescription('Van remény')
